8aaf0b50c5a1dfe6cd27b460a4ac4c12f274343f
Mariya Lagun <108588991+mmarryll@users.noreply.github.com>
2022-12-05 16:47:35 +0300

    Update HomeAutomationRemote.java

--- HomeAutomationRemote.java
+++ HomeAutomationRemote.java
@@ -1,17 +1,12 @@
-
-//Remote Control for Home automation where it will accept the command and execute. 
- 
+//Центр управления
 public class HomeAutomationRemote {
-
-    //Command Holder
+ 
     ICommand command;
 
-    //Set the command in runtime to trigger.
     public void setCommand(ICommand command) {
         this.command = command;
     }
 
-    //Will call the command interface method so that particular command can be invoked.
     public void buttonPressed() {
         command.execute();
     }
