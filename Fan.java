c84ffac0b2b007d021adfbc961d204467099de19
Mariya Lagun <108588991+mmarryll@users.noreply.github.com>
2022-12-05 16:45:59 +0300

    Update Fan.java

--- Fan.java
+++ Fan.java
@@ -1,11 +1,12 @@
+//Вентилятор
 public class Fan {
     void start() {
-        System.out.println("Fan Started..");
+        System.out.println("Fan started.");
 
     }
 
     void stop() {
-        System.out.println("Fan stopped..");
+        System.out.println("Fan stopped.");
 
     }
 }
