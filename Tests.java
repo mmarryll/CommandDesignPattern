a387d92290972184219332f875e25afe10fa4512
Mariya Lagun <108588991+mmarryll@users.noreply.github.com>
2022-12-05 16:44:15 +0300

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class Tests {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @Test
+    public void test1() {
+        Light livingRoomLight = new Light();
+        HomeAutomationRemote remote = new HomeAutomationRemote();
+        remote.setCommand(new TurnOnCommand( livingRoomLight ));
+        remote.buttonPressed();
+        Assertions.assertEquals("Turning on light.\nLight is on.\n", output.toString());
+    }
+
+    @Test
+    public void test2() {
+        Light livingRoomLight = new Light();
+        HomeAutomationRemote remote = new HomeAutomationRemote();
+        remote.setCommand(new TurnOffCommand( livingRoomLight ));
+        remote.buttonPressed();
+        Assertions.assertEquals("Turning off light.\nLight is off.", output.toString());
+    }
+
+    @Test
+    public void test3() {
+        Fan livingRoomFan = new Fan();
+        HomeAutomationRemote remote = new HomeAutomationRemote();
+        remote.setCommand(new StartFCommand( livingRoomFan ));
+        remote.buttonPressed();
+        Assertions.assertEquals("Starting fan.\nFan started.", output.toString());
+    }
+
+    @Test
+    public void test4() {
+        Fan livingRoomFan = new Fan();
+        HomeAutomationRemote remote = new HomeAutomationRemote();
+        remote.setCommand(new StopFCommand( livingRoomFan ));
+        remote.buttonPressed();
+        Assertions.assertEquals("Stopping fan.\nFan stopped.", output.toString());
+    }
+
+    @AfterEach
+    public void cleanUpStreams() {
+        System.setOut(standardOut);
+    }
+}
